755b79abfbc2dfe3c3fda1b6ae514c16b38770c1
RhuanCruz <rhuanvictor883@gmail.com>
2025-12-13 18:56:40 -0300

    feat: Introduce `InteractiveHoverButton` and `SocialSelector` components, update `app/page` with new UI elements, and add `@smoothui` registry.

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import { Input } from "@/components/ui/input";
 import { TextAnimate } from "@/components/ui/text-animate"
 
 import { WordRotate } from "@/components/ui/word-rotate"
+import SocialSelector from "@/components/smoothui/social-selector"
 
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center gap-8 py-32 px-16  sm:items-start z-10">
         <div className="flex flex-col gap-1">
+
           <div className="flex gap-2 justify-start items-center">
             <TextAnimate animation="blurInUp" by="word" className="text-4xl">
               Analytics simples para
@@ -37,6 +39,7 @@ export default function Home() {
           <Input type='email' placeholder="Email" className="w-full bg-white" />
           <Button variant="outline">Inscrever</Button>
         </div>
+        <SocialSelector />
 
       </main>
     </div >
